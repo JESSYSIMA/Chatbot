26b343c74060a07ed6987689acf912c7c443e27b
Yasmine Sidi Mansour <yasmine.sidimansour@univ-constantine2.dz>
2025-11-25 22:33:20 +0100

    first commit

---
+++ chat/views.py
@@ -0,0 +1,30 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from transformers import pipeline
+
+
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+translator = pipeline("translation_en_to_fr")
+
+@api_view(['POST'])
+def chat(request):
+    text = request.data.get("text", "").strip()
+    mode = request.data.get("mode", "").strip()  # "translate" ou "summarize"
+
+    if not text:
+        return Response({"response": "Veuillez fournir du texte."})
+
+    if mode == "translate":
+        reply = translator(text)[0]['translation_text']
+    elif mode == "summarize":
+        if len(text.split()) < 10:
+            reply = " ".join(text.split()[:10]) + " ..."
+        else:
+            words = len(text.split())
+            max_len = min(140, words)
+            min_len = min(70, max_len)
+            reply = summarizer(text, min_length=min_len, max_length=max_len, do_sample=False)[0]['summary_text']
+    else:
+        reply = "Mode inconnu"
+
+    return Response({"response": reply})
